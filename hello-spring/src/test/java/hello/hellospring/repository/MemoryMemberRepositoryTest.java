a74558f4c160debb4ce392496e74897f6e8c934c
brixxt27 <brixxt27@gmail.com>
2023-09-30 03:03:24 +0900

    feat: test code 작성
    * MemoryMemberRepositoryTest
    - @AfterEach 라는 annotation 을 이용해 테스트 메소드가 끝난 이후에 꼭 실행하게 되는 메소드를 생성했다. 왜냐하면 각 테스트를 진행 중에는 같은 메모리를 사용하기 때문에 기존에 변경한 사항이 이후에도 동일하게 적용 되기 때문이다.
    - 그래서 repository 에 clearStore 라는 메소드를 만들어 map 을 clear 하도록 만들었다.
    - assertThat 에는 결과값을 넣고, isEqualTo 에는 기대 되는 값을 넣는다.
    - findById() 를 제외한 테스트 코드를 모두 작성

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,13 +1,21 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
-import org.assertj.core.api.Assertions;
+//import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
     private MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
 
     @Test
     public void save() {
@@ -19,7 +27,45 @@ class MemoryMemberRepositoryTest {
         result = this.repository.findById(member.getId()).get();
 //        System.out.println("result = " + (result == member));
 //        Assertions.assertEquals(result, member);
-        assertThat(member).isEqualTo(result);
-//        assertThat(member).isEqualTo(null);
+        assertThat(result).isEqualTo(member);
+//        assertThat(null).isEqualTo(member);
     }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        Member member2 = new Member();
+
+        member1.setName("spring1");
+        repository.save(member1);
+
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+
+        member2.setName("spring1");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+//    @Test
+//    public void findById() {
+//
+//    }
 }
