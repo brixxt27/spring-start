5ea665a26ffc353c12466a8723811cf3763d8897
brixxt27 <brixxt27@gmail.com>
2023-09-30 23:08:47 +0900

    feat: MemberService 구현 완료
    * MemberService
    - 해당 class 는 Controller 에서 직접적으로 다루는 비즈니스 로직에 대해 다룬다. 대표적으로 repository 를 다루는 로직이 여기에 들어가 있다.
    - 서비스 메소드의 네이밍은 최대한 비즈니스에서 사용하는 단어로 사용하고, repository 에서는 간단명료하게 네이밍을 하는 게 관습이다. 보통 서비스가 주요 로직이니 이를 보고 기능에 대해 이야기하기 때문이다.
    - extract method 를 사용하여 서비스에 길게 작성된 로직을 private 메소드로 쉽게 뺄 수 있다. 이렇게 하면 가독성이 올라가고, 유지 보수가 쉬워진다.
    - 여기서 처음으로 final 키워드를 멤버변수에 사용해봤는데 이는 const 의 의미와 비슷했다. 비슷하다 표현한 이유는 const 는 선언과 동시에 초기화를 해야 하는데 final 은 한 번 할당하면 그 값을 변경하지 않는다는 의미를 갖고 있기 때문이다.
    - 지금까지는 optional 로 반환되는 값을 바로 get() 메소드를 호출하여 그 값을 가져왔는데 이는 바람직한 방법이 아니다. Optional 객체에서 ifPresent 메소드를 호출하면 해당 optional 객체 내부의 값을 가져와서 이 값이 있다면, 콜백 함수를 실행하도록 할 수 있다.
    - 이후 실행한 콜백함수에서는 IllegalStateException 이라는 이름의 예외를 던졌다. 예외는 throw new ~() 으로 던지면 된다.

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // final 은 한 번 할당 되면 더이상 변경 되지 않는다는 의미이다.
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicatedName(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+
+    private void validateDuplicatedName(Member member) { // cmd + b 는 현재 파일에서 해당 메소드를 사용한 곳이나 선언한 곳으로 이동
+        memberRepository.findByName(member.getName())
+                .ifPresent(duplicatedNameMember -> {
+                    throw new IllegalStateException(duplicatedNameMember.getName() + " 은(는) 이미 존재하는 닉네임입니다.");
+                });
+    }
+
+    /**
+     * 모든 멤버 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * member id 로 특정 멤버 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
