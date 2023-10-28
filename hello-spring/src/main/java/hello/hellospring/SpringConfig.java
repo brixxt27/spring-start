9d8bda87eda4e396fd8484deb2724ee7705e9de5
brixxt27 <brixxt27@gmail.com>
2023-10-29 04:46:43 +0900

    feat: Jdbc 사용 위해 DataSource 의존성 주입 받기
    - @Autowired를 통해 Datasource 의존성을 주입 받는다. 이는 JdbcMemberRepository 에서 쿼리를 날리기 위해 사용된다.
    - 기존 Bean에 연결하기 위한 memberRepository 핸들러가 변경된다. 기존에는 MemoryMemberRepository 를 반환하며 해당 객체를 Bean에 등록했는데 지금은 다형성을 이용하여 다른 구현체인 JdbcMemberRepository 를 반환 한다.
    - 해당 repository 에 dataSource 객체가 필요하여 여기에서 DataSource DI가 필요했던 것이다.

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,7 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
-
 }
