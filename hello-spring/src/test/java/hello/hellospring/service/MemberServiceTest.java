9c47c2f029a5b2ecbc1838c04fa394069a226959
brixxt27 <brixxt27@gmail.com>
2023-10-30 00:56:54 +0900

    feat: 통합 테스트 구현 및 완료
    * 통합, 단위 테스트
    - 통합 테스트란 Spring container, DB 를 이용한 테스트라고 한다.
    - 단위 테스트란 단순히 메모리를 통해 한 테스트라고 한다.
    - 통합 테스트는 단위 테스트 보다 훨씬 무겁다. 때문에 적은 테스트라면 상관 없지만 수가 많아질 시 시간은 수 배로 차이가 난다.
    - 보통 단위 테스트로 작은 단위로 하는 테스트가 더 좋은 테스트일 확률이 높다.
    
    * 의존성 주입(DI)
    - 테스트 코드는 외부에서 사용하지 않는다. 때문에 굳이 생성자를 통한 DI 를 하지 않아도 된다.
    - DI

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,43 +1,45 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class MemberServiceTest {
-
-/*
 // memberService 는 내부적으로 MemoryMemberRepository 를 가지고 있다.
 // 하지만 ServiceTest 에서 Repository 를 사용하기 위해 해당 클래스 내에 MemoryMemberRepository 를 생성해줘야 한다.
 // Service 내에서 Repository 를 직접 꺼내는 기능이 없기 때문이다.
 // 단순히 아래처럼 진행해도 결과는 정상적으로 나오지만 엄밀히 이야기하면 서로 다른 repository 를 사용하기 때문에 설계가 변겯 되면 이후 에러가 생길 수 있다.
 // 결과가 정상적으로 나오는 이유는 repository 내의 store 라는 Map 변수가 static 으로 설정 되어 모든 클래스가 이 변수를 공유하기 때문이다.
 // 그래서 이를 해결하기 위해선 DI(Dependency injection) 을 사용하면 쉽게 해결할 수 있다.
+@SpringBootTest
+@Transactional // 테스트는 반복적으로 할 수 있어야 한다. Transaction 을 생성하여 이를 DB에 반영하지 않고 롤백 시킨다.
+class MemberServiceTest {
+
+/*
     private final MemberService memberService = new MemberService();
     MemoryMemberRepository memberRepository = new MemoryMemberRepository();
 */
 
 //    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
 //    MemberService memberService = new MemberService(memberRepository);
-    private MemoryMemberRepository memberRepository;
-    private MemberService memberService;
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private MemberService memberService;
 
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach
-    void afterEach() {
-        memberRepository.clearStore();
-    }
+    // 이제는 스프링 컨테이너에게 객체를 달라고 해야 한다. DI를 통해 객체를 가져오는데 대표적으로 생성자 방식과 필드 방식이 있다.
+//    @BeforeEach
+//    public void beforeEach() {
+//        memberRepository = new MemoryMemberRepository();
+//        memberService = new MemberService(memberRepository);
+//    }
 
     @Test
     void 회원가입() {
