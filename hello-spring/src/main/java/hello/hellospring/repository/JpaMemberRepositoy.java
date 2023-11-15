e47db524673dd4048c81c597a204cf341a18e9a2
brixxt27 <brixxt27@gmail.com>
2023-11-15 14:05:31 +0900

    feat: JpaMemberRepository 구현 완료
    - MemberRepository 인터페이스를 상속 받아서 구현 완료
    - JPA 는 EntityManager 를 통해 구현 한다.
    - save 같은 경우에는 persist() 로 영속성 컨텍스트에 저장한다. 이는 DB에 저장 시키는 게 아니고, 해당 트랜젝션이 끝날 때 commit 을 하면 비로소 DB 에 저장 된다.

---
+++ hello-spring/src/main/java/hello/hellospring/repository/JpaMemberRepositoy.java
@@ -0,0 +1,40 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+
+public class JpaMemberRepositoy implements MemberRepository {
+    private final EntityManager em;
+
+    public JpaMemberRepositoy(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> members = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+        return members.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+}
