e2102ba6695b2832e18a0c899d7596eb4b27730a
brixxt27 <brixxt27@gmail.com>
2023-12-03 19:19:12 +0900

    feat: MemberRepository을 주입 받기 위해 Spring Bean에 등록
    - SpringConfig 생성자를 통해 MemberRepository를 주입 받는다.
    - 기존에는 MemberRepository를 직접 주입 받기 위해 빈에 등록하려고 @Repository(컴포넌트 스캔) 방식을 사용 하지 않고 SpringConfig에 등록하는 방식을 사용했다. 그러나 지금은 JpaRepository를 상속 받음으로써 자동으로 @Repository를 사용한다.

--- hello-spring/src/main/java/hello/hellospring/config/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/config/SpringConfig.java
@@ -1,9 +1,10 @@
-package hello.hellospring;
+package hello.hellospring.config;
 
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.SpringDataJpaMemberRepository;
 import hello.hellospring.service.MemberService;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +13,27 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    private DataSource dataSource;
+//    private DataSource dataSource;
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
-
+//        return new JdbcTemplateMemberRepository(dataSource);
+//    }
 }
