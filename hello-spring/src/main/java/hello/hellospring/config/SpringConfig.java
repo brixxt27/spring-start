117a1a21ded9bcdc1788c92e21d6f9e92170bfbb
brixxt27 <brixxt27@gmail.com>
2023-12-06 17:52:12 +0900

    feat: 기존에 @Configuration 방식으로 빈에 등록하던 MemberService를 컴포넌트 스캔 방식으로 변경하고, TimeTraceAop를 추가함.

--- hello-spring/src/main/java/hello/hellospring/config/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/config/SpringConfig.java
@@ -1,39 +1,13 @@
 package hello.hellospring.config;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import hello.hellospring.repository.SpringDataJpaMemberRepository;
-import hello.hellospring.service.MemberService;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+import hello.hellospring.aop.TimeTraceAop;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-//    private DataSource dataSource;
-//    @Autowired
-//    public SpringConfig(DataSource dataSource) {
-//        this.dataSource = dataSource;
-//    }
-    private final MemberRepository memberRepository;
-
-    @Autowired
-    public SpringConfig(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
     @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository);
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
     }
-
-//    @Bean
-//    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-//    }
 }
